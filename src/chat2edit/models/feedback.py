0af417f881883e1b06ce8fa6bece02ea68751f06
nghialt3670 <nghialt3670@gmail.com>
2025-11-03 22:51:40 +0700

    fix: remove feedback text

--- src/chat2edit/models/feedback.py
+++ src/chat2edit/models/feedback.py
@@ -2,9 +2,9 @@ from typing import Literal, Optional
 
 from pydantic import Field
 
-from chat2edit.models.message import Message
+from chat2edit.models.timestamped_model import TimestampedModel
 
 
-class Feedback(Message):
+class Feedback(TimestampedModel):
     severity: Literal["info", "warning", "error"]
     function: Optional[str] = Field(default=None)
