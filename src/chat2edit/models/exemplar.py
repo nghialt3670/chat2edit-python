3d353b46ab39b05b721b3419b7d726681adeb044
Lê Tín Nghĩa <nghialt3670@192.168.2.4>
2025-12-07 15:17:37 +0700

    fix: update imports

--- src/chat2edit/models/exemplar.py
+++ src/chat2edit/models/exemplar.py
@@ -2,7 +2,7 @@ from typing import List
 
 from pydantic import BaseModel
 
-from chat2edit.models import ExemplaryChatCycle
+from chat2edit.models.exemplary_chat_cycle import ExemplaryChatCycle
 
 
 class Exemplar(BaseModel):
